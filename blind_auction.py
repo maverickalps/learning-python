21dc855a709391b9be189165b2076e8be6330374
deepak <140873736+maverickalps@users.noreply.github.com>
2024-12-20 22:25:16 +0530

    Add files via upload

--- blind_auction.py
+++ blind_auction.py
@@ -1,27 +1,14 @@
+import art
+
 print("Welcome to the secret auction program.")
-bidders = {}
-sample_bidders = {
-    "John": 100,
-    "Jane": 200,
-    "Jack": 300,
-    "deepak": 400,
-    "nims": 56569
-}
-
-name = input("what is your name? : ")
-
-bid = int(input("What is your bid? : $"))
-
-should_continue = input("Are there any other bidders? Type 'yes' or 'no'.").lower()
-print("\n" * 100)
-while should_continue == "yes":
-    bidders[name] = bid
-    name = input("what is your name? : ")
-    bid = int(input("What is your bid? : $"))
-    should_continue = input("Are there any other bidders? Type 'yes' or 'no':  ").lower()
-    print("\n" * 90)
-print(bidders)
-
+print(art.logo)
+# sample_bidders = {
+#     "John": 100,
+#     "Jane": 200,
+#     "Jack": 300,
+#     "deepak": 400,
+#     "nims": 56569
+# }
 def bid_winner(bidders):
     max = 0
     winner = ""
@@ -30,7 +17,27 @@ def bid_winner(bidders):
             max = bidders[key]
             winner = key
     print(f"The winner is {winner} with a bid of ${max}")
-    print(bidders)
+# name = input("what is your name? : ")
+bidders = {}
+# bid = int(input("What is your bid? : $"))
+
+# should_continue = input("Are there any other bidders? Type 'yes' or 'no'.").lower()
+# print("\n" * 100)
+continue_bidding = True
+while continue_bidding: 
+    name = input("what is your name? : ")
+    bid = int(input("What is your bid? : $"))
+    bidders[name] = bid
+    should_continue = input("Are there any other bidders? Type 'yes' or 'no':  ").lower()
+    print("\n" * 90)
+    if should_continue == "no":
+        continue_bidding = False
+    elif should_continue == "yes":
+        continue_bidding = True
+
+print(bidders)
+
+
+    # print(bidders)
 bid_winner(bidders)
-bid_winner(sample_bidders)
-input()
+# bid_winner(sample_bidders)
